af5a2f960ef06bfabc005d12bcec9daf14a4b55e
saikumar.d <saikumardasari8179@gmail.com>
2026-04-14 19:14:29 +0530

    removed

--- rate-public-cloud/src/main/java/com/saparate/pc/repository/PCArtifactVersionRepo.java
+++
@@ -1,8 +0,0 @@
-package com.saparate.pc.repository;
-
-import com.saparate.pc.entity.PCArtifactVersion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PCArtifactVersionRepo extends JpaRepository<PCArtifactVersion, Long> {
-
-}
